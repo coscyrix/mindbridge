06fa090f791991690d0a8e0e04a03ce7651bc2c2
RaymondChitauro <137214620+RaymondChitauro@users.noreply.github.com>
2025-01-24 13:23:29 -0600

    Remove authentication middleware from feedback routes for streamlined access

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -6,51 +6,51 @@ import { authenticate } from '../middlewares/token.js';
 const router = Router();
 const feedbackController = new FeedbackController();
 
-router.post('/', authenticate, AsyncWrapper(feedbackController.postFeedback));
+router.post('/', AsyncWrapper(feedbackController.postFeedback));
 
-router.put('/', authenticate, AsyncWrapper(feedbackController.putFeedbackById));
+router.put('/', AsyncWrapper(feedbackController.putFeedbackById));
 
-router.get('/', authenticate, AsyncWrapper(feedbackController.getFeedbackById));
+router.get('/', AsyncWrapper(feedbackController.getFeedbackById));
 
 router.post(
   '/gad7',
-  authenticate,
+
   AsyncWrapper(feedbackController.postGAD7Feedback),
 );
 
 router.post(
   '/phq9',
-  authenticate,
+
   AsyncWrapper(feedbackController.postPHQ9Feedback),
 );
 
 router.post(
   '/pcl5',
-  authenticate,
+
   AsyncWrapper(feedbackController.postPCL5Feedback),
 );
 
 router.post(
   '/whodas',
-  authenticate,
+
   AsyncWrapper(feedbackController.postWHODASFeedback),
 );
 
 router.post(
   '/ipf',
-  authenticate,
+
   AsyncWrapper(feedbackController.postIPFSFeedback),
 );
 
 router.post(
   '/smart-goal',
-  authenticate,
+
   AsyncWrapper(feedbackController.postSMARTGOALFeedback),
 );
 
 router.post(
   '/consent',
-  authenticate,
+
   AsyncWrapper(feedbackController.postCONSENTFeedback),
 );
 
