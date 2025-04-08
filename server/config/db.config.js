c2b81302e65913615cfd518baf93f23d972cf02a
RaymondChitauro <137214620+RaymondChitauro@users.noreply.github.com>
2025-04-07 21:07:07 -0500

    Refactor database connection settings to standardize pool configurations and improve connection timeout settings

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -16,12 +16,34 @@ const dbConn = {
       dateStrings: true,
     },
     pool: {
-      max: 100,
+      max: 50,
       min: 0,
-      //acquire: 30000,
-      //idle: 10000,
+      idleTimeoutMillis: 10000, // Supported Tarn.js option
+      acquireTimeoutMillis: 20000, // Supported Tarn.js option
     },
+    acquireConnectionTimeout: 10000, // Wait up to 10 seconds to establish a connection
     debug: true,
+  },
+  production: {
+    client: 'mysql2',
+    connection: {
+      host: process.env.MYSQL_URL,
+      user: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      port: process.env.MYSQL_PORT,
+      database: process.env.MYSQL_DATABASE,
+      multipleStatements: true,
+      timezone: 'Z',
+      dateStrings: true,
+    },
+    pool: {
+      max: 50,
+      min: 0,
+      idleTimeoutMillis: 10000, // Supported Tarn.js option
+      acquireTimeoutMillis: 20000, // Supported Tarn.js option
+    },
+    acquireConnectionTimeout: 10000, // Wait up to 10 seconds to establish a connection
+    debug: false,
   },
 };
 
